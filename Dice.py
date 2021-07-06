8f87d16584de84e79f61a32ca9007ed047383f30
wt128 <s1280120@u-aizu.ac.jp>
2021-07-07 02:49:27 +0900

    Initial revision

---
+++ Dice.py
@@ -0,0 +1,8 @@
+import random
+print("Rolling the dice...")
+d1 = random.randint(1,6)
+d2 = random.randint(1,6)
+sum = d1 + d2
+print("Die 1: "+str(d1))
+print("Die 2: "+str(d2))
+print("Total value: "+str(sum)) 
